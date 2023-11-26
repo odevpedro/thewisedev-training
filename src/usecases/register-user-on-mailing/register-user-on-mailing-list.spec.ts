4aedae51bac128f8d5ddfa755eca79981999c9ca
odevpedro <pedrosschmidt2@gmail.com>
2023-11-26 12:24:48 -0300

    feat: user data interface

--- src/usecases/register-user-on-mailing/register-user-on-mailing-list.spec.ts
+++ src/usecases/register-user-on-mailing/register-user-on-mailing-list.spec.ts
@@ -1,5 +1,9 @@
+// Definindo os testes de alto nivel da nossa aplicação
+import { UserData } from './user-data'
+
 describe('Just testing if my jest is working', () => {
-  test('', () => {
-    expect(1).toBe(1)
+  test('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+    console.log(users)
   })
 })
