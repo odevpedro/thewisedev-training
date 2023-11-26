a9d9f3a95cb67317e58fb9c79a9eb4d3a4ac3405
odevpedro <pedrosschmidt2@gmail.com>
2023-11-26 12:25:33 -0300

    test: tdd defining high-level tests

--- src/usecases/register-user-on-mailing/register-user-on-mailing-list.spec.ts
+++ src/usecases/register-user-on-mailing/register-user-on-mailing-list.spec.ts
@@ -5,5 +5,12 @@ describe('Just testing if my jest is working', () => {
   test('should add user with complete data to mailing list', async () => {
     const users: UserData[] = []
     console.log(users)
+    // const repo: UserRepository = new InMemoryUserRepository(users)
+    // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    // const name = "any_name"
+    // const email = "any@email.com"
+    // const reponse = await usecase.registerUserOnMailingList({ name, email })
+    // const user = repo.findUserByEmail('any@email.com')
+    // expect((await user).name).toBe('any_name')
   })
 })
