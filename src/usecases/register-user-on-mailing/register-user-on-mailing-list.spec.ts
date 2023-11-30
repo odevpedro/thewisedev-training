292d92d478a8c1138ac9e53a20221d989f520404
odevpedro <pedrosschmidt2@gmail.com>
2023-11-30 16:35:55 -0300

    test: fix test name

--- src/usecases/register-user-on-mailing/register-user-on-mailing-list.spec.ts
+++ src/usecases/register-user-on-mailing/register-user-on-mailing-list.spec.ts
@@ -1,7 +1,7 @@
 // Definindo os testes de alto nivel da nossa aplicação
 import { UserData } from './user-data'
 
-describe('Just testing if my jest is working', () => {
+describe('Register user on mailing list use case', () => {
   test('should add user with complete data to mailing list', async () => {
     const users: UserData[] = []
     console.log(users)
